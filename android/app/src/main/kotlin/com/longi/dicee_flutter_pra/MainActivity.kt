b122d4558022c374f5ea460e1d62ee1fcd803b82
admin <ingkeanglong@gmail.com>
2024-11-08 10:36:31 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/longi/dicee_flutter_pra/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.longi.dicee_flutter_pra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              'Dicee',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 45.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Doto'),
+            ),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: const Column(
+          children: [
+            Divider(
+              height: 1.0,
+              // thickness: 2,
+              color: Colors.white,
+            ),
+            Expanded(
+              child:  DicePage(),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
